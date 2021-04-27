dcee0cb2ea8575f0f99a6f1d441d836e180072fb
Nathan da Silva <pacsmacck@gmail.com>
2021-04-27 04:15:16 -0700

    final version

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,23 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
+
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language)
+                console.log(data)
+            })
+        }
+        else {
+            alert("Error: GitHub user not found")
+        }
+    })
+}
 
 var formSubmitHandler = function (event) {
     // prevent page from refreshing
@@ -90,5 +107,15 @@ var displayRepos = function (repos, searchTerm) {
     }
 };
 
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language)
+    }
+
+    repoContainerEl.textContent = "";
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
